23a9aac9a14a4880f1859774908d69bd9c241d0d
mary-munya0 <maryjunemunyao@gmail.com>
2024-12-19 15:50:54 +0300

    web-store-api-final

---
+++ GameStore.Api/Program.cs
@@ -0,0 +1,17 @@
+using GameStore.Api.Data;
+using GameStore.Api.Endpoints;
+// using GameStore.Api.Endpoints;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connString = builder.Configuration.GetConnectionString("GameStore");
+builder.Services.AddSqlite<GameStoreContext>(connString);
+
+var app = builder.Build();
+
+app.MapGamesEndpoints();
+app.MapGenreEndpoints();
+
+await app.MigrateDbAsync();
+
+app.Run();
